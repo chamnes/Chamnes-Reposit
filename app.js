09f29ed9cdf958b3eff5dd350bd41351a28024d8
chamnes <pichchamnes@gmail.com>
2024-01-12 01:35:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var pug = require('pug');
+var routes = require('./routes/index');
 
 var app = express();
 
 const PORT = process.env.SERVER_PORT;
 
-
+app.use('/', routes);
 
 const menus = [
   { name: 'Vanilla', description: 'A plain vanilla ice cream' },
@@ -24,3 +31,6 @@ app.get('/menu', (req, res, next) => {
 app.listen(PORT, function () {
     console.log('Simple Web Application running on port ' + PORT);
 });
+
+
+module.exports = app;
