13fcbb9d9ee5b78b29be2bca8536d21536d2baa9
chamnes <pichchamnes@gmail.com>
2024-01-11 22:28:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ var app = express();
 
 const PORT = process.env.SERVER_PORT;
 
+const menus = [
+  { name: 'Vanilla', description: 'A plain vanilla ice cream' },
+  { name: 'Choco Chips', description: 'A chocolate chips ice cream' },
+];
+
 app.get('/', function (req, res) {
   res.send('Simple Web Application is UP');
+});
+
+app.get('/menu', (req, res, next) => {
+  res.json({
+    data: menus,
+  });
 });
 
 app.listen(PORT, function () {
