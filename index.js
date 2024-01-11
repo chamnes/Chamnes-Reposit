d7d2503db7ea682feb69ead075ab5cdb93d1d1df
chamnes <pichchamnes@gmail.com>
2024-01-11 22:50:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var app = express();
 
 const PORT = process.env.SERVER_PORT;
 
+const pgp = require('pg-promise')(/* options */)
+const db = pgp('postgres://uaykdeachphmq477qihr:oEYmQyGTZwet69srUPsDPlJMy4irOG@bkl4kp4n4z4ybkhhavrf-postgresql.services.clever-cloud.com:50013/bkl4kp4n4z4ybkhhavrf')
+
+db.one('SELECT $1 AS value', 123)
+  .then((data) => {
+    console.log('DATA:', data.value)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+
 const menus = [
   { name: 'Vanilla', description: 'A plain vanilla ice cream' },
   { name: 'Choco Chips', description: 'A chocolate chips ice cream' },
