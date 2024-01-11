a3dee795b9373a59fb2da264d8a1cd4fdf350240
chamnes <pichchamnes@gmail.com>
2024-01-12 01:27:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,7 @@ var app = express();
 
 const PORT = process.env.SERVER_PORT;
 
-var router = express.Router();
 
-var db = require('../queries');
-
-
-router.get('/api/puppies', db.getAllPuppies);
-router.get('/api/puppies/:id', db.getSinglePuppy);
-router.post('/api/puppies', db.createPuppy);
-router.put('/api/puppies/:id', db.updatePuppy);
-router.delete('/api/puppies/:id', db.removePuppy);
-
-
-module.exports = router;
 
 const menus = [
   { name: 'Vanilla', description: 'A plain vanilla ice cream' },
